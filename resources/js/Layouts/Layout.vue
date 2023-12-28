2ad4e3ff4f1d893c46c323f8a36e2bcbbc4ce9aa
chanthornAcademy <hoeurnchanthorn@gmail.com>
2023-12-28 16:15:12 +0700

    ...

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Header from "@/Layouts/Header.vue";
+import Sidebar from "@/Layouts/Sidebar.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+<template>
+    <Head>
+        <title>Dasboard</title>
+    </Head>
+
+    <div class="flex h-screen p-3 bg-blue-700 dark:bg-blue-900">
+        <Sidebar />
+        <div class="flex flex-col flex-1 overflow-hidden">
+            <Header />
+            <main
+                id="full-screen-main"
+                class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 dark:bg-slate-800 scroll-smooth rounded-b-2xl dark:text-white"
+                scroll-region
+            >
+                <div>
+                    <slot />
+                </div>
+            </main>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -1,28 +1,15 @@
 <script setup lang="ts">
-import Header from "@/Layouts/Header.vue";
-import Sidebar from "@/Layouts/Sidebar.vue";
-import { Head } from "@inertiajs/vue3";
-import { ref } from "vue";
+import Layout from "@/Layouts/Layout.vue";
 </script>
-<template>
-    <Head>
-        <title>Dasboard</title>
-    </Head>
 
-    <div class="flex h-screen p-3 bg-blue-700 dark:bg-blue-900">
-        <Sidebar />
-        <div class="flex flex-col flex-1 overflow-hidden">
-            
-            <Header />
-            <main
-                id="full-screen-main"
-                class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 dark:bg-slate-800 scroll-smooth rounded-b-2xl dark:text-white"
-                scroll-region
-            >
-                <div>
-                    <slot />
-                </div>
-            </main>
+<template>
+    <Layout>
+        <div class="p-4">
+            <div class="p-2 bg-white dark:bg-gray-800 rounded-xl">
+                <h1 class="text-2xl font-bold text-gray-800 dark:text-white">
+                    Dashboard
+                </h1>
+            </div>
         </div>
-    </div>
+    </Layout>
 </template>
